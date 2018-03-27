013ec26d5ccfe2934a43a02c31bcfe98f486526c
Cody A. Coleman <codyaustun@gmail.com>
2018-03-27 13:58:57 -0700

    Add more tests as sanity checks

--- tests/test_submissions.py
+++ tests/test_submissions.py
@@ -1,10 +1,12 @@
 import os
 import json
 from glob import glob
+from datetime import datetime
 from itertools import chain
 from collections import defaultdict
 
 import pytest
+import pandas as pd
 
 
 DATASETS = ['CIFAR10', 'ImageNet', 'SQuAD']
@@ -38,3 +40,91 @@ def test_train_file_is_valid_json(path):
 def test_infer_file_is_valid_json(path):
     with open(path) as json_file:
         json.load(json_file)
+
+
+@pytest.mark.parametrize(
+    "path",
+    files['train'],
+    ids=[os.path.split(path)[1] for path in files['train']]
+)
+def test_train_file_has_required_fields(path):
+    with open(path) as json_file:
+        record = json.load(json_file)
+
+    required_fields = [
+        'version', 'author', 'authorEmail', 'framework',
+        'model', 'hardware', 'timestamp'
+    ]
+
+    for field in required_fields:
+        assert field in record, "{} is a required field".format(field)
+
+    # Enforce timestamp format
+    datetime.strptime(record['timestamp'], '%Y-%m-%d')
+
+
+@pytest.mark.parametrize(
+    "path",
+    files['infer'],
+    ids=[os.path.split(path)[1] for path in files['infer']]
+)
+def test_infer_file_has_required_fields(path):
+    with open(path) as json_file:
+        record = json.load(json_file)
+
+    required_fields = [
+        'version', 'author', 'authorEmail', 'framework',
+        'model', 'hardware', 'timestamp', 'latency'
+    ]
+
+    for field in required_fields:
+        assert field in record, "{} is a required field".format(field)
+
+    # Enforce timestamp format
+    datetime.strptime(record['timestamp'], '%Y-%m-%d')
+
+
+@pytest.mark.parametrize(
+    "path",
+    files['CIFAR10']['train'],
+    ids=[os.path.split(path)[1] for path in files['CIFAR10']['train']]
+)
+def test_cifar10_train_file_has_valid_tsv(path):
+    data_path = os.path.splitext(path)[0] + '.tsv'
+    assert os.path.exists(data_path), "The TSV file doesn't exist"
+
+    df = pd.read_csv(data_path, sep='\t')
+    required_columns = ['epoch', 'hours', 'top1Accuracy']
+    assert sorted(df.columns) == sorted(required_columns), "Incorrect columns"
+
+    assert len(df) > 0, "The TSV file shouldn't be empty"
+
+
+@pytest.mark.parametrize(
+    "path",
+    files['ImageNet']['train'],
+    ids=[os.path.split(path)[1] for path in files['ImageNet']['train']]
+)
+def test_imagenet_train_file_has_valid_tsv(path):
+    data_path = os.path.splitext(path)[0] + '.tsv'
+    assert os.path.exists(data_path), "The TSV file doesn't exist"
+
+    df = pd.read_csv(data_path, sep='\t')
+    required_columns = ['epoch', 'hours', 'top1Accuracy', 'top5Accuracy']
+    assert sorted(df.columns) == sorted(required_columns), "Incorrect columns"
+
+    assert len(df) > 0, "The TSV file shouldn't be empty"
+
+
+@pytest.mark.parametrize(
+    "path",
+    files['SQuAD']['train'],
+    ids=[os.path.split(path)[1] for path in files['SQuAD']['train']]
+)
+def test_squad_train_file_has_valid_tsv(path):
+    data_path = os.path.splitext(path)[0] + '.tsv'
+    assert os.path.exists(data_path), "The TSV file doesn't exist"
+
+    df = pd.read_csv(data_path, sep='\t')
+    required_columns = ['epoch', 'hours', 'f1Score']
+    assert sorted(df.columns) == sorted(required_columns)
